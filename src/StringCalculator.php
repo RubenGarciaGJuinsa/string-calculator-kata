da0d7b13bef86e856a37b6399c72448fdda53b36
Rubén García <rubengarcia@grupojuinsa.es>
2020-02-03 15:38:03 +0100

    GREEN: pass_separator_in_number_string

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -8,7 +8,17 @@ class StringCalculator
 {
     public static function add(string $numbers): int
     {
-        $explodedNumbers = preg_split('/(,|\n)/', $numbers);
+        $splitChars = ',|\n';
+
+        preg_match('/^(\\/\\/(.)+\\n)?((.|\\n)+)/', $numbers, $matches);
+        if ( ! empty($matches[2])) {
+            $splitChars .= '|'.$matches[2];
+        }
+        if ( ! empty($matches[3])) {
+            $numbers = $matches[3];
+        }
+
+        $explodedNumbers = preg_split('/('.$splitChars.')/', $numbers);
         $result = array_sum($explodedNumbers);
 
         return $result;
