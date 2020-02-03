849dc7e82d34036c3881f8c560f91c42b395dad1
Rubén García <rubengarcia@grupojuinsa.es>
2020-02-03 14:53:04 +0100

    BLUE: pass_two_numbers_and_get_the_sum

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -8,7 +8,6 @@ class StringCalculator
 {
     public static function add(string $numbers): int
     {
-        $result = 0;
         $explodedNumbers = explode(',', $numbers);
         $result = array_sum($explodedNumbers);
 
